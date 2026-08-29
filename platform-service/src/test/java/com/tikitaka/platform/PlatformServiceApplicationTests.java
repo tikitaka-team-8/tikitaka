a87f8d568fee533fb35d6c6f475a9f382e139196
choin <choin0330@gmail.com>
2026-08-29 21:28:24 +0900

    [Chore] 프로젝트 초기 개발 환경 구성 (#2)
    * chore: IntelliJ 및 개발 환경 제외 설정 추가
    
    * docs: 이슈 및 PR 템플릿 추가
    
    * chore: Spring Boot 초기 구성

---
+++ platform-service/src/test/java/com/tikitaka/platform/PlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikitaka.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
