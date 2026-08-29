5ca2bbae652c0d224a3912574935684f0b841fe4
choin <choin0330@gmail.com>
2026-08-29 21:12:43 +0900

    chore: Spring Boot 초기 구성

---
+++ payment-notification-service/src/main/java/com/tikitaka/paymentnotification/PaymentNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tikitaka.paymentnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ payment-notification-service/src/test/java/com/tikitaka/paymentnotification/PaymentNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikitaka.paymentnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
