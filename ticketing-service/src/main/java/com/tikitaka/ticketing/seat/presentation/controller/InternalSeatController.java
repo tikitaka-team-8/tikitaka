0864afaf273d3cc70119bdf9702e84dc982a90ee
juwonyoung <dnjsdud0702@naver.com>
2026-09-07 21:14:02 +0900

    feat : 좌석 선점 해제 내부연동 컨트롤러 연결

---
+++ ticketing-service/src/main/java/com/tikitaka/ticketing/seat/presentation/controller/InternalSeatController.java
@@ -0,0 +1,39 @@
+package com.tikitaka.ticketing.seat.presentation.controller;
+
+
+import com.tikitaka.ticketing.seat.application.service.SeatService;
+import com.tikitaka.ticketing.seat.domain.enums.ReleaseReason;
+import com.tikitaka.ticketing.seat.presentation.dto.request.SeatReleaseRequest;
+import com.tikitaka.ticketing.seat.presentation.security.InternalAuthValidator;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/internal/")
+@RequiredArgsConstructor
+public class InternalSeatController {
+
+    private final  SeatService seatService;
+    private final InternalAuthValidator internalAuthValidator;
+
+    private static final String INTERNAL_AUTH_HEADER = "X-Internal-Auth";
+
+    @PostMapping("/seat-holds/{seatHoldId}/release")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public  void releaseHold(
+            @PathVariable UUID seatHoldId,
+            @RequestHeader(INTERNAL_AUTH_HEADER) @NotBlank String internalAuthToken,
+            @Valid @RequestBody SeatReleaseRequest request
+    ) {
+        internalAuthValidator.validate(internalAuthToken);
+        ReleaseReason reason = ReleaseReason.valueOf(request.reason());
+
+        seatService.releaseHold(seatHoldId, reason);
+
+    }
+}
